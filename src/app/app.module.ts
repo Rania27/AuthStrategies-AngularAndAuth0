45f33a075cbaecd4e6af0091c351b8c31e904667
Rania Bouker <rania.bouker@gmail.com>
2018-03-19 13:33:20 +0100

    First Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
+import { AuthService } from './services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+
+import { ROUTES } from './app.routes'
 import { AppComponent } from './app.component';
+import { CallbackComponent } from './callback/callback.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CallbackComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+ 
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
